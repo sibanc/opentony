15cad5e7133068cd7a9e8b5f0491958c0bb94d85
sibanc <rok.sibanc@gmail.com>
2023-12-17 22:53:32 +0100

    ok test

---
+++ readme.md
@@ -0,0 +1,16 @@
+# OpenTONY
+Tunes Only Now Yours
+
+## Based on
+- https://github.com/pschatzmann/arduino-audiokit
+- https://github.com/pschatzmann/arduino-audio-tools
+
+
+## Todos
+- [ ] rfid
+- [ ] playlist
+- [ ] upload files
+
+## References
+https://github.com/pschatzmann/arduino-audiokit/wiki/Problems-with-the-SD-Drive-or-some-conflicting-Buttons
+https://github.com/espressif/arduino-esp32/tree/master/libraries/SD_MMC
--- src/main.cpp
+++ src/main.cpp
@@ -1,118 +1,43 @@
-#include "Arduino.h"
-#include "WiFi.h"
-#include "Audio.h"
-#include "FS.h"
-#include "SD_MMC.h"
-
-/*
- * pin 1 - D2                |  Micro SD card     |
- * pin 2 - D3                |                   /
- * pin 3 - CMD               |                  |__
- * pin 4 - VDD (3.3V)        |                    |
- * pin 5 - CLK               | 8 7 6 5 4 3 2 1   /
- * pin 6 - VSS (GND)         | ▄ ▄ ▄ ▄ ▄ ▄ ▄ ▄  /
- * pin 7 - D0                | ▀ ▀ █ ▀ █ ▀ ▀ ▀ |
- * pin 8 - D1                |_________________|
- *                             ║ ║ ║ ║ ║ ║ ║ ║
- *                     ╔═══════╝ ║ ║ ║ ║ ║ ║ ╚═════════╗
- *                     ║         ║ ║ ║ ║ ║ ╚══════╗    ║
- *                     ║   ╔═════╝ ║ ║ ║ ╚═════╗  ║    ║
- * Connections for     ║   ║   ╔═══╩═║═║═══╗   ║  ║    ║
- * full-sized          ║   ║   ║   ╔═╝ ║   ║   ║  ║    ║
- * SD card             ║   ║   ║   ║   ║   ║   ║  ║    ║
- * ESP32-S3 DevKit  | 21  47  GND  39 3V3 GND  40 41  42  |
- * ESP32-S3-USB-OTG | 38  37  GND  36 3V3 GND  35 34  33  |
- * ESP32            |  4   2  GND  14 3V3 GND  15 13  12  |
- * Pin name         | D1  D0  VSS CLK VDD VSS CMD D3  D2  |
- * SD pin number    |  8   7   6   5   4   3   2   1   9 /
- *                  |                                  █/
- *                  |__▍___▊___█___█___█___█___█___█___/
- * WARNING: ALL data pins must be pulled up to 3.3V with an external 10k Ohm resistor!
- * Note to ESP32 pin 2 (D0): Add a 1K Ohm pull-up resistor to 3.3V after flashing
- *
- * SD Card | ESP32
- *    D2       12
- *    D3       13
- *    CMD      15
- *    VSS      GND
- *    VDD      3.3V
- *    CLK      14
- *    VSS      GND
- *    D0       2  (add 1K pull up after flashing)
- *    D1       4
- *
- *    For more info see file README.md in this library or on URL:
- *    https://github.com/espressif/arduino-esp32/tree/master/libraries/SD_MMC
+/**
+ * @file player-sd-audiokit.ino
+ * @brief see https://github.com/pschatzmann/arduino-audio-tools/blob/main/examples/examples-audiokit/player-sdmmc-audiokit/README.md
+ * Make sure that the pins are set to on, on, on, on, on
+ * @author Phil Schatzmann
+ * @copyright GPLv3
  */
 
+#include "AudioTools.h"
+#include "AudioLibs/AudioKit.h"
+#include "AudioLibs/AudioSourceIdxSDMMC.h" // or AudioSourceIdxSDMMC.h
+#include "AudioCodecs/CodecMP3Helix.h"
 
-void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
-    Serial.printf("Listing directory: %s\n", dirname);
-
-    File root = fs.open(dirname);
-    if(!root){
-        Serial.println("Failed to open directory");
-        return;
-    }
-    if(!root.isDirectory()){
-        Serial.println("Not a directory");
-        return;
-    }
-
-    File file = root.openNextFile();
-    while(file){
-        if(file.isDirectory()){
-            Serial.print("  DIR : ");
-            Serial.println(file.name());
-            if(levels){
-                listDir(fs, file.path(), levels -1);
-            }
-        } else {
-            Serial.print("  FILE: ");
-            Serial.print(file.name());
-            Serial.print("  SIZE: ");
-            Serial.println(file.size());
-        }
-        file = root.openNextFile();
-    }
-}
+const char *startFilePath="/";
+const char* ext="mp3";
+AudioSourceIdxSDMMC source(startFilePath, ext);
+AudioKitStream kit;
+MP3DecoderHelix decoder;  // or change to MP3DecoderMAD
+AudioPlayer player(source, kit, decoder);
 
 void setup() {
-    Serial.begin(115200);
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial, AudioLogger::Info);
 
-    SD_MMC.setPins(14, 15, 2, 4, 12, 13);
+  // setup output
+  auto cfg = kit.defaultConfig(TX_MODE);
+  cfg.sd_active = false;
+  kit.begin(cfg);
 
-  if(!SD_MMC.begin("/sdcard",false, false)){
-      Serial.println("Card Mount Failed");
-      return;
-  }
-  uint8_t cardType = SD_MMC.cardType();
 
-  if(cardType == CARD_NONE){
-      Serial.println("No SD_MMC card attached");
-      return;
-  }
+  // setup player
+  player.setVolume(0.7);
+  player.begin();
 
-  Serial.print("SD_MMC Card Type: ");
-  if(cardType == CARD_MMC){
-      Serial.println("MMC");
-  } else if(cardType == CARD_SD){
-      Serial.println("SDSC");
-  } else if(cardType == CARD_SDHC){
-      Serial.println("SDHC");
-  } else {
-      Serial.println("UNKNOWN");
-  }
-
-  uint64_t cardSize = SD_MMC.cardSize() / (1024 * 1024);
-    Serial.printf("SD_MMC Card Size: %lluMB\n", cardSize);
-
-    listDir(SD_MMC, "/", 0);
-
-  Serial.println("OpenTONY");
+  // select file with setPath() or setIndex()
+  //player.setPath("/ZZ Top/Unknown Album/Lowrider.mp3");
+  player.setIndex(1); // 2nd file
 
 }
 
 void loop() {
-
+  player.copy();
 }
