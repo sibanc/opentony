8ae6545480a043f710008534e137f9c9df672044
sibanc <rok.sibanc@gmail.com>
2023-12-18 23:40:52 +0100

    playlist, sim RFID by serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /**
  * @file player-sd-audiokit.ino
  * @brief see https://github.com/pschatzmann/arduino-audio-tools/blob/main/examples/examples-audiokit/player-sdmmc-audiokit/README.md
- * Make sure that the pins are set to on, on, on, on, on
+ * Make sure that the pins are set to on, on, on, on, on (Rok ?? off on on off off works)
  * @author Phil Schatzmann
  * @copyright GPLv3
  */
@@ -17,6 +17,26 @@ AudioSourceIdxSDMMC source(startFilePath, ext);
 AudioKitStream kit;
 MP3DecoderHelix decoder;  // or change to MP3DecoderMAD
 AudioPlayer player(source, kit, decoder);
+
+const int BUFFER_SIZE = 100;
+char buf[BUFFER_SIZE];
+
+int volume = 7;
+
+
+
+bool isDirectory(fs::FS &fs, const char * path){
+ File root = fs.open(path);
+ if(!root){
+   //Serial.println("Failed to open directory");
+   return false;
+ }
+ if(!root.isDirectory()){
+   //Serial.println("Not a directory");
+   return false;
+ }
+ return true;
+}
 
 
 void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
@@ -50,8 +70,6 @@ void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
     }
 }
 
-int volume = 7;
-
 void volume_up(bool, int, void*){
   if(volume < 10){
     volume++;
@@ -66,12 +84,14 @@ void volume_down(bool, int, void*){
   }
 }
 
-void next(bool, int, void*) {
-   player.next();
+void rfid1(bool, int, void*) {
+  source.setPath("/RFID1");
+  player.begin();
 }
 
-void previous(bool, int, void*) {
-   player.previous();
+void rfid2(bool, int, void*) {
+  source.setPath("/RFID2");
+  player.begin();
 }
 
 void startStop(bool, int, void*) {
@@ -80,7 +100,7 @@ void startStop(bool, int, void*) {
 
 void setup() {
   Serial.begin(115200);
-  AudioLogger::instance().begin(Serial, AudioLogger::Info);
+  AudioLogger::instance().begin(Serial, AudioLogger::Warning);
 
   // setup output
   auto cfg = kit.defaultConfig(TX_MODE);
@@ -89,8 +109,8 @@ void setup() {
 
  // setup additional buttons 
   kit.addAction(PIN_KEY1, startStop);
-  kit.addAction(PIN_KEY4, next);
-  kit.addAction(PIN_KEY3, previous);
+  kit.addAction(PIN_KEY4, rfid1);
+  kit.addAction(PIN_KEY3, rfid2);
   kit.addAction(PIN_KEY5, volume_down);
   kit.addAction(PIN_KEY6, volume_up);
 
@@ -106,7 +126,36 @@ void setup() {
 
 }
 
+void playlist(const char* dir){
+  if(isDirectory(SD_MMC,dir)){
+    source.setPath(dir);
+    player.begin();
+  }
+}
+
+
+
+void processSerial(){
+  if (Serial.available() > 0) {
+
+    //todo: replace with nonblocking readbytes... process when last char is \n or \r ...
+    int rlen = Serial.readBytesUntil('\n', buf, BUFFER_SIZE);
+
+    switch(buf[0]){
+      case 'L':
+        buf[1] = '/';
+        playlist(&buf[1]);
+        break;
+
+      default:
+        return;
+    }
+  }
+}
+
 void loop() {
   player.copy();
   kit.processActions();
+
+  processSerial();
 }
