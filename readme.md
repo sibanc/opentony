646659ab8418becceaf3dde8b0c1f63e38930b4e
sibanc <rok.sibanc@gmail.com>
2023-12-18 14:55:35 +0100

    updated readme

--- readme.md
+++ readme.md
@@ -1,15 +1,23 @@
 # OpenTONY
 Tunes Only Now Yours
 
-## Based on
+## Hardware
+[ESP32 Audio Kit from Ai Thinker](https://docs.ai-thinker.com/en/esp32-audio-kit)
+[schematic](https://docs.ai-thinker.com/_media/esp32-audio-kit_v2.2_sch.pdf)
+
+current S1 DIP switch selections: 0 1 1 0 0, which also works for 4bit SD mode (using SDMMC library)
+
+## Firmware
+based on
 - https://github.com/pschatzmann/arduino-audiokit
 - https://github.com/pschatzmann/arduino-audio-tools
 
 
 ## Todos
 - [ ] rfid
-- [ ] playlist
+- [ ] playlists
 - [ ] upload files
+- [ ] auto limit amount of playtime per day
 
 ## References
 https://github.com/pschatzmann/arduino-audiokit/wiki/Problems-with-the-SD-Drive-or-some-conflicting-Buttons
--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,54 @@ AudioKitStream kit;
 MP3DecoderHelix decoder;  // or change to MP3DecoderMAD
 AudioPlayer player(source, kit, decoder);
 
+
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+    Serial.printf("Listing directory: %s\n", dirname);
+
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("Failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println("Not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.path(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("  SIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+int volume = 7;
+
+void volume_up(bool, int, void*){
+  if(volume < 10){
+    volume++;
+    player.setVolume(volume/10.0);
+  }
+}
+
+void volume_down(bool, int, void*){
+  if(volume > 0){
+    volume--;
+    player.setVolume(volume/10.0);
+  }
+}
+
 void next(bool, int, void*) {
    player.next();
 }
@@ -43,14 +91,18 @@ void setup() {
   kit.addAction(PIN_KEY1, startStop);
   kit.addAction(PIN_KEY4, next);
   kit.addAction(PIN_KEY3, previous);
+  kit.addAction(PIN_KEY5, volume_down);
+  kit.addAction(PIN_KEY6, volume_up);
 
   // setup player
-  player.setVolume(0.7);
+  player.setVolume(volume/10.0);
   player.begin();
 
   // select file with setPath() or setIndex()
   //player.setPath("/ZZ Top/Unknown Album/Lowrider.mp3");
   player.setIndex(0); // 2nd file
+
+  listDir(SD_MMC,"/",1);
 
 }
 
